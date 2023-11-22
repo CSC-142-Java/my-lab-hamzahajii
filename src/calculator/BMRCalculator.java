3ceecfb32597e03e10af81a41b734c4c7248efaa
hamzahajii <hamzahaji21@icloud.com>
2023-11-22 10:45:03 -0800

    BMR calculator update

--- src/calculator/BMRCalculator.java
+++ src/calculator/BMRCalculator.java
@@ -1,17 +1,36 @@
 package calculator;
 
+import java.util.Scanner;
 public class BMRCalculator {
 
     public static double computeBMI(double weight, int height) {
-        return 0.0;
+
+        return (double) 703 * weight / (height * height) ;
     }
 
     public static double computeBMR(int age, char bodyType, double weight, int height) {
-        return 0.0;
+        double bmrValue = 4.54545 * weight + 15.875 * height - 5 * age;
+        if (bodyType == 'F')
+            return bmrValue + 5;
+        else
+            return bmrValue - 161;
     }
 
     public static void main(String[] args) {
         //TODO: USe the design document on Canvas to complete this program
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name age body type weight and height in the space below");
+        String name = scanner.next();
+        int age = scanner.nextInt();
+        String bType = scanner.next();
+        char bodyType = bType.toUpperCase().charAt(0); //Taking the first letter of conversion to char
+        double weight = scanner.nextDouble();
+        int height = scanner.nextInt();
+
+        System.out.printf("Your BMI is %5.2f \n", computeBMI(weight, height));
+        System.out.printf("Your BMR is %5.2f \n", computeBMR(age, bodyType, weight, height));
+
+
     }
 
 }
